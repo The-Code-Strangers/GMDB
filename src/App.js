91362ec8f12d2967157513e84a3fbc10bca08f3b
Christian Sonntag <Christian@Christians-Air.fritz.box>
2020-03-31 14:09:32 +0200

    Test client

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 import './App.css';
 
+function getMovies(){
+  fetch("http://localhost:8080/movies").then((response)=>console.log(response.json()));
+}
+
 function App() {
+  getMovies();
   return (
     <div className="App">
      
